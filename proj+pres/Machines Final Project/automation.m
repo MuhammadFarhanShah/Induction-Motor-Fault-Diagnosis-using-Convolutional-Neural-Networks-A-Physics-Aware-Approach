d82d4fa514f2d4269f8c60327f56d5c7ccdfc5bb
Muhammad Farhan Shah <mshah.bee23seecs@seecs.edu.pk>
2026-09-19 18:55:11 +0500

    Add files via upload

---
+++ proj+pres/Machines Final Project/automation.m
@@ -0,0 +1,7 @@
+disp('Running first script...');
+parallel_samples_generation;  % Replace with your actual filename
+
+disp('Running second script...');
+cnn_training4;  % Replace with your actual filename
+
+disp('All done.');
